40b5a08ea071e3451f9d23544bd1dc0740dd38db
Apollo23 <85381172+Apollo23SMO@users.noreply.github.com>
2021-10-30 15:05:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 const Body = Matter.Body;
-const Compositites;
+const Composites = Matter.Composites;
 const Composite = Matter.Composite;
 
 let engine;
